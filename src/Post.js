2e3f9786452c607416449795c6fd62dc63324c22
Vikash Kumar <vikasthakur9310@gmail.com>
2023-06-29 23:59:31 +0530

    add

--- src/Post.js
+++ src/Post.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 const Post = () => {
   return (
-    <div>
+    <div className='post'>
           <h3>username</h3>   
           {/* header-avatar + username */}
 
 
           {/* image */}
-          <img src='https://images.unsplash.com/photo-1508341591423-4347099e1f19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80' alt='postimage' />
+          <img className='post__image' src='https://images.unsplash.com/photo-1508341591423-4347099e1f19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80' alt='postimage' />
           {/* username+caption */}
+
+          <h4> username : caption </h4>
     </div>
   )
 }
