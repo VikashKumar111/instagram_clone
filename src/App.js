459e3e47d9abab371d60b7c4f0c77f49e49446f2
Vikash Kumar <vikasthakur9310@gmail.com>
2023-07-01 23:58:50 +0530

    add

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
       <h1>Hello clever programmers lets build an instagram clone using react</h1>
       <Post username="clever qazi" caption="day three of live sessions awesome" imagrUrl="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
-      <Post username="vikas thakur" caption="welldone brother u have done it"/>
+      <Post username="vikas thakur" caption="welldone brother u have done it" imageUrl=" https://img.freepik.com/premium-photo/full-length-portrait-handsome-young-man-posing-isolated-gray-wall_171337-83295.jpg?w=2000"/>
       <Post username="saurabh chauhan" caption="enjoying the holidays "/>
       <Post username="abhishek thakur" caption="turkey is awesome country"/>
       
--- src/Post.js
+++ src/Post.js
@@ -16,7 +16,7 @@ const Post = ({username ,caption ,imageUrl}) => {
           
 
           
-          <img className='post__image' src='https://images.unsplash.com/photo-1508341591423-4347099e1f19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80' alt='postimage' />
+          <img className='post__image' src={imageUrl} alt='postimage' />
           
 
           <h4 className='post__text'> <strong>CleverQazi :</strong> {caption} </h4>
