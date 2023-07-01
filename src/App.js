461ba929209ff6b48e4ba067214826c2114776de
Vikash Kumar <vikasthakur9310@gmail.com>
2023-07-02 00:11:26 +0530

    add

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ function App() {
       </div>
 
       <h1>Hello clever programmers lets build an instagram clone using react</h1>
-      <Post username="clever qazi" caption="day three of live sessions awesome" imagrUrl="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
-      <Post username="vikas thakur" caption="welldone brother u have done it" imageUrl=" https://img.freepik.com/premium-photo/full-length-portrait-handsome-young-man-posing-isolated-gray-wall_171337-83295.jpg?w=2000"/>
-      <Post username="saurabh chauhan" caption="enjoying the holidays "/>
-      <Post username="abhishek thakur" caption="turkey is awesome country"/>
+      <Post username="clever qazi" caption="day three of live sessions awesome" imageUrl="https://i.pinimg.com/564x/5f/88/6a/5f886abc01c18bc33b58ff5be07c2e79.jpg" />
+      <Post username="vikas thakur" caption="welldone brother u have done it" imageUrl="https://i.pinimg.com/564x/f0/cd/89/f0cd89de60fa627f422a1b6d8880e79a.jpg"/>
+      <Post username="saurabh chauhan" caption="enjoying the holidays " imageUrl="https://i.pinimg.com/564x/97/f1/a5/97f1a5510f7f2268f412e73a666410d4.jpg" />
+      <Post username="abhishek thakur" caption="turkey is awesome country" imageUrl="https://i.pinimg.com/564x/71/56/3b/71563b077c5722a896abaa9e6c0b1c32.jpg" />
       
 
 
