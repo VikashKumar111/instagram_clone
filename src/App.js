ee91ac7f8d34438db039a313e486abc9ddcd8c6a
Vikash Kumar <vikasthakur9310@gmail.com>
2023-07-04 19:18:47 +0530

    add

--- src/App.js
+++ src/App.js
@@ -3,10 +3,32 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Post from './Post';
 import { db } from './firebase';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { Input } from '@mui/material';
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 
 function App() {
-  const [posts, setPosts] = useState([])
-  
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
@@ -16,11 +38,54 @@ function App() {
     });
     
   } , [])
-
+  
+  const signUp = (event) => {
+    
+  }
 
   return (
     <div className="app">
       
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <Box sx={style}>
+          <center>
+            <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+            />
+          </center>
+          
+            <Input
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <Input
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <Input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <Button onClick={signUp}>Sign Up</Button>
+            
+          
+        </Box>
+      </Modal>
+
       <div className="app__header">
         <img
                 className="app__headerImage"
@@ -28,7 +93,8 @@ function App() {
                 alt=""
               />
       </div>
-
+      
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
       <h1>Hello clever programmers lets build an instagram clone using react</h1>
 
       {
