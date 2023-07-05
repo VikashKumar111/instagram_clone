d21ef72aceafe445b17e3b3bb7dfc273667c9aa3
Vikash Kumar <vikasthakur9310@gmail.com>
2023-07-05 22:58:48 +0530

    add

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,7 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import { storage , db } from './firebase';
 
 const ImageUpload = () => {
     const [caption, setCaption] = useState('');
+    const [image, setImage] = useState(null);
+    const [progress, setProgress] = useState(0);
+
+    const handleChange = (e) => {
+        if (e.target.files[0]) {
+            setImage(e.target.files[0]);
+        }
+    };
+
+    const handleUpload = () => {
+        const uploadTask = storage.ref(`images/${image.name}`).put(image);
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                // progress function
+                const progress = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+                setProgress(progress);  
+            },
+            (error) => {
+                //  error message
+                console.log(error);
+                alert(error.message);
+            },
+            
+            
+
+        )
+        
+    };
 
   return (
       <div>
